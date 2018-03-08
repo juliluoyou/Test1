6702e48dd5a83f9ac5951f552a2ed9912c03bc08
juliluoyou <rainingliu0@gmail.com>
2018-03-08 23:02:14 +0800

    added druid

--- src/main/java/com/example/myblogdev/WebdemoApplication.java
+++ src/main/java/com/example/myblogdev/WebdemoApplication.java
@@ -3,8 +3,10 @@ package com.example.myblogdev;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("com.example.myblogdev.mapper")
 public class WebdemoApplication {
 
---
+++ src/main/resources/readme.txt
@@ -0,0 +1,2 @@
+Druid: http://blog.csdn.net/aqsunkai/article/details/69660817?locationNum=2&fps=1
+	url: http://localhost:8080/druid/index.html
