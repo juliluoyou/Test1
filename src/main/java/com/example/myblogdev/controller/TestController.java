4021c3ef42a23459857b3ae7d2501e145965f680
juliluoyou <rainingliu0@gmail.com>
2018-03-08 23:37:29 +0800

    added thymeleaf

--- src/main/java/com/example/myblogdev/controller/TestController.java
+++ src/main/java/com/example/myblogdev/controller/TestController.java
@@ -1,17 +1,25 @@
 package com.example.myblogdev.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.myblogdev.entity.t_attach;
 import com.example.myblogdev.service.impl.TestService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/")
 public class TestController {
+	
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());  
+    protected final ObjectMapper objectMapper = new ObjectMapper();  
 	
 	@Resource
 	TestService testService;
@@ -19,8 +27,18 @@ public class TestController {
 	@ResponseBody
 	@RequestMapping("/t")
 	public t_attach test(){
+		logger.trace("I am trace log.");  
+        logger.debug("I am debug log.");  
+        logger.warn("I am warn log.");  
+        logger.error("I am error log.");  
 		t_attach t1 = testService.test(1);
 		return t1;
+	}
+	
+	@RequestMapping("index")
+	public String index(Map<String, Object> map){
+		map.put("hello", "this is a thymeleaf test");
+        return "/index";
 	}
 
 }
--- src/main/resources/readme.txt
+++ src/main/resources/readme.txt
@@ -1,2 +1,10 @@
-Druid: http://blog.csdn.net/aqsunkai/article/details/69660817?locationNum=2&fps=1
-	url: http://localhost:8080/druid/index.html
+Druid: 
+		http://blog.csdn.net/aqsunkai/article/details/69660817?locationNum=2&fps=1
+--url: 	http://localhost:8080/druid/index.html
+		https://github.com/alibaba/druid/tree/master/druid-spring-boot-starter
+
+log4j2:(参考使用xml即可) http://blog.csdn.net/clementad/article/details/51498864
+
+thymeleaf: 
+	https://www.jianshu.com/p/0cb218be0e7a
+	https://www.cnblogs.com/studyDetail/p/6995458.html
